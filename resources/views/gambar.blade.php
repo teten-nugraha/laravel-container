04008c6fd05c02917a2ce0c4cfe33af4247028cc
Teten Nugraha <teten.nugraha18@gmail.com>
2026-04-10 08:41:02 +0700

    s

---
+++ resources/views/gambar.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Gambar</title>
+</head>
+<body>
+
+    <h1>Ini Gambar dari Laravel</h1>
+
+    <img src="{{ asset('images/luffy.png') }}" alt="Sample Image" width="400">
+
+</body>
+</html>
