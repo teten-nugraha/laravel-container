8323cc8105ef191c150a6868fa666cd3bda66dcc
Teten Nugraha <teten.nugraha18@gmail.com>
2026-04-10 10:37:12 +0700

    update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,10 +6,12 @@ if [ ! -f /var/www/vendor/autoload.php ]; then
     cp -a /opt/vendor/. /var/www/vendor/
 fi
 
-mkdir -p /var/www/storage/logs /var/www/bootstrap/cache
-chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+mkdir -p /var/www/storage/logs /var/www/storage/framework /var/www/bootstrap/cache
+chown -R www-data:www-data /var/www/storage/logs /var/www/storage/framework /var/www/bootstrap/cache
+chown www-data:www-data /var/www/storage /var/www/storage/app
 chown -R www-data:www-data /var/www/vendor
 chmod -R ug+rwX /var/www/vendor
-chmod -R ug+rwX /var/www/storage /var/www/bootstrap/cache
+chmod -R ug+rwX /var/www/storage/logs /var/www/storage/framework /var/www/bootstrap/cache
+chmod ug+rwX /var/www/storage /var/www/storage/app
 
 exec "$@"
